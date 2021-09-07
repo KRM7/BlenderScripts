ecdda78cc97e2990921925616da18188a78178a8
KRM7 <70973547+KRM7@users.noreply.github.com>
2021-09-07 21:35:01 +0200

    image output format updated
    -file structure -> index, updateable
    -file extension -> PNG
    -lighting options
    -defect generation done
    -render engine choice

--- generate.py
+++ generate.py
@@ -9,14 +9,14 @@ import math
 import random
 import time
 import configparser
-import haircomb, scene, shaders, render, utils
+import csv
+import numpy as np
+import haircomb, scene, shaders, render, utils, defects
 
 
 config = configparser.ConfigParser()
 config.read(os.path.join(project_path, "config.ini"))
 
-
-start_time = time.time()
 
 # Clear everything in the scene
 bpy.ops.object.select_all(action = "SELECT")
@@ -26,21 +26,24 @@ utils.removeMaterials()
 utils.removeLights()
 utils.removeCameras()
 
-# Set the render settings
-render.setupCycles(samples = int(config["Render"]["samples"]),
-                   bounces = int(config["Render"]["bounces"]),
-                   tile_size = int(config["Render"]["tile_size"]),
-                   denoising = config["Render"]["denoise"] == "True")
+# Set the render settings based on the config
+if config["Render"]["engine"] == "cycles":
+    render.setupCycles(samples = int(config["Render"]["samples"]),
+                       bounces = int(config["Render"]["bounces"]),
+                       tile_size = int(config["Render"]["tile_size"]),
+                       denoising = config["Render"]["denoise"] == "True")
+elif config["Render"]["engine"] == "eevee":
+    render.setupEevee(samples = int(config["Render"]["samples"]))
 
 
-# Set the output image settings
+# Set the output image settings based on the config
 render.setImageSettings(res_x = int(config["Image"]["resolution_x"]),
                         res_y = int(config["Image"]["resolution_y"]),
                         color = config["Image"]["color"] == "True")
 
 
 # Setup the environment (the parts which won't change between images)
-# Ground
+# Ground (only the shaders change between images, the object doesn't)
 ground_mat = bpy.data.materials.new(name = "ground")
 bpy.ops.mesh.primitive_plane_add(size = scene.ground_plane_size)
 ground = bpy.context.object
@@ -48,55 +51,75 @@ ground.data.materials.append(ground_mat)
 
 # World/environment
 world = bpy.context.scene.world
-node_env = world.node_tree.nodes.new(type = "ShaderNodeTexEnvironment")
-world.node_tree.links.new(node_env.outputs["Color"], world.node_tree.nodes["Background"].inputs["Color"])
+if config["Lights"]["use_hdris"] == "True":
+    node_env = world.node_tree.nodes.new(type = "ShaderNodeTexEnvironment")
+    world.node_tree.links.new(node_env.outputs["Color"], world.node_tree.nodes["Background"].inputs["Color"])
+
+# Set up the defect combination generator
+defect_gen = defects.Defects(config["Object"]["enable_missing_teeth"] == "True",
+                              config["Object"]["enable_bent_teeth"] == "True",
+                              config["Object"]["enable_warping"] == "True",
+                              config["Object"]["enable_ejector_marks"] == "True",
+                              config["Object"]["enable_gloss"] == "True",
+                              config["Object"]["enable_discoloration"] == "True",
+                              config["Object"]["enable_contamination"] == "True",
+                              config["Object"]["enable_cloudy"] == "True",
+                              config["Object"]["enable_splay"] == "True",
+                              int(config["Object"]["num_ejector_marks"]))
 
 
-# Classes
+# Index file
+img_name, img_cntr = 0, 0
+index_filepath = os.path.join(config["Paths"]["output_path"], "index.csv")
 
+append_index = os.path.isfile(index_filepath) and config["Output"]["overwrite"] != "True"
+if append_index:
+    index_file = open(index_filepath, "r")
+    img_name = sum(1 for line in index_file)
+    index_file.close()
 
+if append_index:
+    index_file = open(index_filepath, "a+", newline = "")
+    index_writer = csv.writer(index_file)
+else:
+    index_file = open(index_filepath, "w", newline = "")
+    index_writer = csv.writer(index_file)
+    index_writer.writerow(["Filenames", "missing_teeth", "bent_teeth", "warped", "ejector_marks", "low_gloss", "discoloration", "contamination", "cloudy", "splay"])
 
-# Create the objects
-imgs_per_class = int(config["Output"]["imgs_per_class"])
+# The overall number of images to generate, and the number of images to generate of 1 object before creating a new model.
+num_imgs = int(config["Output"]["num_imgs"])
 imgs_per_object = int(config["Output"]["imgs_per_object"])
 
-validate_p = float(config["Output"]["validate"])
-test_p = float(config["Output"]["test"])
-
-for obj_idx in range(int(imgs_per_class/imgs_per_object)):
+for _ in range(int(num_imgs/imgs_per_object)):
     
     # Delete the haircomb if it already exists
     if "hc" in locals():
         bpy.data.objects.remove(hc.getObject())
+
+    # Generate a new defect combination
+    defect_gen.updateDefectCombination()
     
     # Create the haircomb with defects if needed
-    hc = haircomb.Haircomb(missing_teeth = False,
-                           bent_teeth = False,
-                           warping = False,
-                           ejector_marks = 0)
+    hc = haircomb.Haircomb(missing_teeth = defect_gen.missing_teeth,
+                           bent_teeth = defect_gen.bent_teeth,
+                           warping = defect_gen.warping,
+                           ejector_marks = defect_gen.ejector_marks)
     hc.createHaircomb()
 
     # Generate images of the haircomb
-    for img_idx in range(int(config["Output"]["imgs_per_object"])):
-
-        img_num = obj_idx * imgs_per_object + (img_idx + 1)
+    for _ in range(imgs_per_object):
 
         # Add random materials
         tex_path = os.path.join(config["Paths"]["project_path"], "textures")
-        # Material of the haircomb
-        #if contamination: defect = "contamination"
-        #elif splay: defect = "splay"
-        #elif cloudy: defect = "cloudy"
-        #elif gloss: defect = "gloss"
-        #elif discoloration: defect = "discoloration"
-        #else : defect = None
 
         shaders.applyPlastic(mat = hc.getMaterial(),
                              color = (0.0, 0.0, 0.0, 1.0),
-                             surface = "Matte",
+                             surface = config["Object"]["surface"],
                              randomize = config["Object"]["randomize"] == "True",
                              textures_path = tex_path,
-                             defect = None)
+                             tex_defect = defect_gen.tex_defect,
+                             gloss_defect = defect_gen.gloss,
+                             discoloration = defect_gen.discoloration)
 
         # Apply material to the ground plane
         ground_texture = shaders.applyRandomTextures(ground_mat, tex_path)
@@ -106,7 +129,9 @@ for obj_idx in range(int(imgs_per_class/imgs_per_object)):
         scene.removeCameras()
 
         coords = hc.getBoundingBox()
-        coords = utils.randomExtendBoundingBox(bounding_box = coords, max_x = hc.width/6, max_y = hc.width/6)
+        coords = utils.randomExtendBoundingBox(bounding_box = coords,
+                                               max_x = hc.width*float(config["Camera"]["extend_x"]),
+                                               max_y = hc.width*float(config["Camera"]["extend_y"]))
 
         camera = scene.placeCamera(coords,
                                    max_view_angle_x = float(config["Camera"]["max_view_angle_x"]),
@@ -115,27 +140,37 @@ for obj_idx in range(int(imgs_per_class/imgs_per_object)):
 
 
         # Lighting setup
-        hdri_idx = random.randrange(len(scene.hdris))
-        hdri = scene.hdris[hdri_idx]
-        hdri_path = os.path.join(config["Paths"]["project_path"], "hdris", hdri["name"])
+        if config["Lights"]["use_hdris"] == "True":
+            hdri = random.choice(scene.hdris)
+            hdri_path = os.path.join(config["Paths"]["project_path"], "hdris", hdri["name"])
 
-        node_env.image = bpy.data.images.load(hdri_path, check_existing = True)
-        world.node_tree.nodes["Background"].inputs["Strength"].default_value = random.uniform(hdri["light_min"], hdri["light_max"])
-
-        # Adjust light based on material
-        world.node_tree.nodes["Background"].inputs["Strength"].default_value += ground_texture["light_correction"]
+            node_env.image = bpy.data.images.load(hdri_path, check_existing = True)
+            world.node_tree.nodes["Background"].inputs["Strength"].default_value = random.uniform(hdri["light_min"], hdri["light_max"])
+            # Adjust HDRI light strength based on the ground textures
+            world.node_tree.nodes["Background"].inputs["Strength"].default_value += ground_texture["light_correction"]
+        else:
+            scene.removeLights()
+            scene.placeLights(num_lights = int(config["Lights"]["num_lights"]),
+                              height_min = float(config["Lights"]["height_min"]),
+                              height_max = float(config["Lights"]["height_max"]),
+                              hdistance_min = float(config["Lights"]["distance_min"]),
+                              hdistance_max = float(config["Lights"]["distance_max"]),
+                              strength_min = float(config["Lights"]["strength_min"]),
+                              strength_max = float(config["Lights"]["strength_max"]))
 
 
         # Render and save the image
-        if (img_num <= (1.0 - validate_p - test_p) * imgs_per_class):
-            dir = "train"
-        elif (img_num <= (1.0 - test_p) * imgs_per_class):
-            dir = "validate"
-        else:
-            dir = "test"
-
-        img_path = os.path.join(config["Paths"]["output_path"], dir, str(img_num))
+        img_path = os.path.join(config["Paths"]["output_path"], "imgs", str(img_name) + ".png")
         render.render(img_path)
 
+        # Add the generated image to the index
+        index_writer.writerow([img_name, str(int(defect_gen.missing_teeth)), str(int(defect_gen.bent_teeth)), str(int(defect_gen.warping)),
+                               str(int(defect_gen.ejector_marks != 0)), str(int(defect_gen.gloss)), str(int(defect_gen.discoloration)),
+                               str(int(defect_gen.contamination)), str(int(defect_gen.cloudy)), str(int(defect_gen.splay))])
 
-print("Overall time: %s seconds" % round((time.time() - start_time), 2))
+        print("Image " + str(img_cntr + 1) + "/" + str(num_imgs) + " done.\n")
+        img_name += 1
+        img_cntr += 1
+
+
+index_file.close()
